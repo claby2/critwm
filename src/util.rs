02b560cfc3aea616a7b80563862d9255c4249b3c
Edward Wibowo <wibow9770@gmail.com>
2021-05-02 12:25:45 +0800

    feat: allow spawning commands with arguments

--- src/util.rs
+++ src/util.rs
@@ -51,8 +51,13 @@ macro_rules! key {
     };
 }
 
-pub fn spawn(program: &str) {
-    Command::new(program).spawn().unwrap();
+pub fn spawn(parts: &str) {
+    let parts = String::from(parts);
+    let mut parts = parts.trim().split_whitespace();
+    Command::new(parts.next().unwrap())
+        .args(parts)
+        .spawn()
+        .unwrap();
 }
 
 pub fn signal(signal: Signal) {
